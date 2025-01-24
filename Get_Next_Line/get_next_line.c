f2991472e04e56623c15b3058b4d266b8b2aa351
Mohamed Zanana <mr.zananamohamed@gmail.com>
2025-01-24 21:04:00 +0100

    create the function file and handle the null return of the failed tries

---
+++ Get_Next_Line/get_next_line.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/24 20:52:30 by mzanana           #+#    #+#             */
+/*   Updated: 2025/01/24 20:52:31 by mzanana          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "get_next_line.h"
+
+char    *get_next_line(int fd)
+{
+    char        *buffer;
+    static char *box;
+    ssize_t     readret;
+
+    buffer = NULL;
+    while (1)
+    {
+        buffer = malloc (sizeof(char) * (ssize_t)BUFFER_SIZE + 1);
+        if (fd < 0 || BUFFER_SIZE <= 0 || !buffer)
+            return (free (buffer), free (box), box = NULL, NULL);
+        readret = read (fd, buffer, BUFFER_SIZE);
+        if (readret == -1)
+            return (free (buffer), free (box), box = NULL, NULL);
+        buffer[readret] = '\0';
+        box = ft_strjoin (box, buffer);
+    }
+}
