19cb5d99193e1eccb479c581c81cd9164b75bfc8
Mohamed Zanana <mr.zananamohamed@gmail.com>
2025-01-24 23:18:35 +0100

    add the functions prototype to thr header

--- Get_Next_Line/get_next_line.h
+++ Get_Next_Line/get_next_line.h
@@ -13,8 +13,15 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <unistd.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 # define BUFFER_SIZE 10
+
+char    *get_next_line(int fd);
+int     ft_strlen(char *str);
+char    *ft_substr(char *src, int from, int to, int flag);
+char    *ft_strncpy(char *dst, char *src, int n);
+char    *ft_strjoin(char *origin, char *added);
+
 #endif
