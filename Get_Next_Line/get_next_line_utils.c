b5d416afeb4cd9fe0b7a58eb41d8fb5e3453cb1b
Mohamed Zanana <mr.zananamohamed@gmail.com>
2025-01-25 02:13:40 +0100

    Utils norminette fixed

--- Get_Next_Line/get_next_line_utils.c
+++ Get_Next_Line/get_next_line_utils.c
@@ -6,72 +6,70 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 21:07:17 by mzanana           #+#    #+#             */
-/*   Updated: 2025/01/24 21:07:19 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/01/25 02:11:43 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "get_next_line.h"
 
-int     ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
-    int     i;
+	int	i;
 
-    if (!str)
-        return (0);
-    i = 0;
-    while (str[i])
-        i++;
-    return (i);
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
 
-char    *ft_substr(char *src, int from, int to, int flag)
+char	*ft_substr(char *src, int from, int to, int flag)
 {
-    char    *ret;
-    int     i;
-    int     length;
+	char	*ret;
+	int		i;
+	int		length;
 
-    length = to - from;
-    ret = malloc (sizeof(char) * (length + 1));
-    if (!ret)
-        return (free (src), NULL);
-    i = 0;
-    while (i < length)
-    {
-        ret[i] = src[i + from];
-        i++;
-    }
-    ret [i] = '\0';
-    if (flag)
-        free(src);
-    return (ret);
+	length = to - from;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (free(src), NULL);
+	i = 0;
+	while (i < length)
+	{
+		ret[i] = src[i + from];
+		i++;
+	}
+	ret[i] = '\0';
+	if (flag)
+		free(src);
+	return (ret);
 }
 
-char    *ft_strncpy(char *dst, char *src, int n)
+char	*ft_strncpy(char *dst, char *src, int n)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i < n && src[i])
-    {
-        dst[i] = src[i];
-        i++;
-    }
-    dst[i] = '\0';
-    return (dst);
+	i = 0;
+	while (i < n && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
 }
 
-int     ft_strchr(char *src, ssize_t *ptr)
+int	ft_strchr(char *src, ssize_t *ptr)
 {
-    if (!src)
-        return (0);
-    *ptr = 0;
-    while (src[(*ptr)])
-    {
-        if (src[(*ptr)] == '\n')
-            return (1);
-        (*ptr)++;
-    }
-    return (0);
+	if (!src)
+		return (0);
+	*ptr = 0;
+	while (src[(*ptr)])
+	{
+		if (src[(*ptr)] == '\n')
+			return (1);
+		(*ptr)++;
+	}
+	return (0);
 }
-
