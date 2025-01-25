8dc8291745229f7a8cc0df7a6b031404e53436a1
Mohamed Zanana <mr.zananamohamed@gmail.com>
2025-01-25 02:14:48 +0100

    header file norminette

--- Get_Next_Line/get_next_line.h
+++ Get_Next_Line/get_next_line.h
@@ -6,15 +6,15 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 20:49:35 by mzanana           #+#    #+#             */
-/*   Updated: 2025/01/25 01:35:54 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/01/25 01:36:48 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
 # include <stdlib.h>
+# include <unistd.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 10
@@ -26,6 +26,5 @@ char	*ft_substr(char *src, int from, int to, int flag);
 char	*ft_strncpy(char *dst, char *src, int n);
 int		ft_strchr(char *src, ssize_t *ptr);
 char	*ft_strjoin(char *origin, char *added);
-
 
 #endif
