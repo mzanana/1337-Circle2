5098e3eb32c7b6cc1148a8defe8cd0534abe2e9c
Mohamed Zanana <mr.zananamohamed@gmail.com>
2025-02-02 15:39:29 +0100

    the header Update

--- Ft_Printf/Project/ft_printf.h
+++ Ft_Printf/Project/ft_printf.h
@@ -7,6 +7,9 @@
 # include <stdlib.h>
 
 int ft_printf(const char *str, ...);
-
+void ft_putchar(char c, int *x);
+void ft_putnbr(int nb, int *x);
+void	ft_putstr(char *str, int *x);
+void    ft_voidhex(void *str, int *x);
 
 # endif
