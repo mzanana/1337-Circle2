04b2f20779f0b6ad01bf785cec1195c4545b51d8
Mohamed Zanana <mr.zananamohamed@gmail.com>
2025-02-04 15:58:29 +0100

    unsigned function added

--- Ft_Printf/Project/ft_printf.h
+++ Ft_Printf/Project/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 15:51:32 by mzanana           #+#    #+#             */
-/*   Updated: 2025/02/04 15:49:19 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/02/04 15:57:56 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int		ft_printf(const char *str, ...);
 void	ft_putchar(char c, int *x);
 void	ft_putnbr(int nb, int *x);
 void	ft_putstr(char *str, int *x);
+void	ft_unsigned(unsigned int nb, int *x);
 void	ft_voidhex(unsigned long nbr, int *x);
 void	ft_hexal(unsigned int nb, int *x, char c);
 int		ft_strchr(char *str, char c);
