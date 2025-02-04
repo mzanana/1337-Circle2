9ccef5327f76a4cb6ff898406a0c0614d4e0f1bc
mzanana <mr.zananamohamed@gmail.com>
2025-02-04 20:25:54 +0000

    After recoding for the second time

--- Ft_Printf/Project/ft_unsigned.c
+++ Ft_Printf/Project/ft_unsigned.c
@@ -12,36 +12,21 @@
 
 #include "ft_printf.h"
 
-int	ft_calcule2(unsigned int nb)
+void	ft_unsigned(unsigned int x, int *y)
 {
-	int	ret;
+	char	str[12];
+	char	*hold;
+	int		i;
 
-	ret = 0;
-	while (nb > 0)
+	if (x == 0)
+        return (ft_putchar('0', y));
+	hold = "0123456789";
+	i = 0;
+	while(x > 0)
 	{
-		nb /= 10;
-		ret++;
+		str[i++] = hold[x % 10];
+		x /= 10;
 	}
-	return (ret);
-}
-
-void	ft_unsigned(unsigned int nb, int *x)
-{
-	int		lenght;
-	char	*str;
-
-	if (nb == 0)
-		return (ft_putchar('0', x));
-	lenght = ft_calcule2(nb);
-	str = malloc(sizeof(char) * (unsigned int)(lenght + 1));
-	if (!str)
-		return ;
-	str[lenght] = '\0';
-	while (--lenght >= 0)
-	{
-		str[lenght] = nb % 10 + 48;
-		nb /= 10;
-	}
-	ft_putstr(str, x);
-	free(str);
+	while (--i >= 0)
+		ft_putchar(str[i], y);
 }
