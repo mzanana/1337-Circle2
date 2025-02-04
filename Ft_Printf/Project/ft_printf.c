5fb42d98a3b26eab7f041bb2ca18e3dd7c0059e5
Mohamed Zanana <mr.zananamohamed@gmail.com>
2025-02-04 15:21:49 +0100

    fix the if else statements

--- Ft_Printf/Project/ft_printf.c
+++ Ft_Printf/Project/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mzanana <mzanana@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 15:51:22 by mzanana           #+#    #+#             */
-/*   Updated: 2025/02/04 15:12:43 by mzanana          ###   ########.fr       */
+/*   Updated: 2025/02/04 15:18:29 by mzanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,14 @@ int	ft_helper(va_list args, char c, int *i)
 {
 	if (c == 'c')
 		ft_putchar(va_arg(args, int), i);
-	if (c == 'd')
+	else if (c == 'd')
 		ft_putnbr(va_arg(args, int), i);
-	if (c == 's')
+	else if (c == 's')
 		ft_putstr(va_arg(args, char*), i);
-	if (c == 'p')
-		ft_voidhex(va_arg(args, void *), i);
-	if (c == '%')
-		ft_putchar(va_arg(args, int), i);
+	// else if (c == 'p')
+	// 	ft_voidhex(va_arg(args, void *), i);
+	else if (c == '%')
+		ft_putchar('%', i);
 	return (*i);
 }
 
