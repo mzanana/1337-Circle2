cff7febd5fcfda43b5f26398b1b5a0a964040f0c
mzanana <mr.zananamohamed@gmail.com>
2025-02-05 13:40:56 +0000

    fix the isue of printf

--- Ft_Printf/Project/ft_printf_bonus.c
+++ Ft_Printf/Project/ft_printf_bonus.c
@@ -26,7 +26,7 @@ int	ft_strchr(char *str, char c)
 	return (0);
 }
 
-int	ft_bonuscheck(va_list args, char flag, char next, int *ret)
+int	ft_bonuscheck(va_list args, char flag, char next, int *ret, int *i)
 {
 	if (flag == '+' && (next == 'd' || next == 'i'))
 		ft_plus(va_arg(args, int), '+', ret);
@@ -38,6 +38,7 @@ int	ft_bonuscheck(va_list args, char flag, char next, int *ret)
 	{
 		ft_hexal(va_arg(args, unsigned int), ret, "0123456789abcdef", 'X');
 	}
+	(*i)++;
 	return (*ret);
 }
 
@@ -79,7 +80,7 @@ int	ft_printf(const char *str, ...)
 		{
 			i++;
 			if (ft_strchr("# +", str[i]) && str[i + 1])
-				ret = ft_bonuscheck(args, str[i], str[i + 1], &ret);
+				ret = ft_bonuscheck(args, str[i], str[i + 1], &ret, &i);
 			else if (ft_strchr("cspdiuxX%", str[i]))
 				ret = ft_helper(args, str[i], &ret);
 		}
